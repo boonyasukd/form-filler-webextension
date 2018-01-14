c422cf7ddd563643e48f7519b2b79deb8c3ddf67
Boonyasuk Dheeravongkit <boonyasuk.d@gmail.com>
2018-01-14 21:44:06 +0700

    misc fixes
    - auto-fill placeholder email
    - correctly add "Sex"
    - support form fill both in reservation create and edit mode

--- popup.js
+++ popup.js
@@ -4,108 +4,114 @@ const capitalizeFirstLetter = (string) => string.charAt(0) + string.slice(1).toL
 const genXpath = (expr) => `document.evaluate('${expr}', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue`;
 
 new Vue({
-    el: '#app',
-    data: {
-        loading: false,
-        invalid: false,
-        currentTab: 'thaiId',
-        mrz: null,
-        passport: null,
-        thaiId: null,
+  el: '#app',
+  data: {
+    loading: false,
+    invalid: false,
+    currentTab: 'thaiId',
+    mrz: null,
+    passport: null,
+    thaiId: null,
+  },
+  watch: {
+    mrz: function (newVal, oldVal) {
+      if (newVal) this.parseMrz();
+    }
+  },
+  methods: {
+    goToPassport: function () {
+      this.reset();
+      this.currentTab = 'passport';
     },
-    watch: {
-        mrz: function(newVal, oldVal) {
-            if (newVal) this.parseMrz();
+    goToThaiId: function () {
+      this.reset();
+      this.currentTab = 'thaiId';
+    },
+    fetchData: async function () {
+      try {
+        this.loading = true;
+        const res = await fetch('http://localhost:8080/card');
+        this.loading = false;
+        if (res.ok) {
+          this.thaiId = await res.json();
+          this.thaiId["DOB"] = formatDate(this.thaiId["DOB"]);
+          this.thaiId["Issue"] = formatDate(this.thaiId["Issue"]);
+          this.thaiId["Expiry"] = formatDate(this.thaiId["Expiry"]);
+          console.log(this.thaiId);
+        } else {
+          throw new Error('Network response was not ok.');
         }
+      } catch (err) {
+        this.invalid = true;
+        this.loading = false;
+        console.log(err);
+      }
     },
-    methods: {
-        goToPassport: function() {
-            this.reset();
-            this.currentTab = 'passport';
-        },
-        goToThaiId: function() {
-            this.reset();
-            this.currentTab = 'thaiId';
-        },
-        fetchData: async function() {
-            try {
-                this.loading = true;
-                const res = await fetch('http://localhost:8080/card');
-                this.loading = false;
-                if (res.ok) {
-                    this.thaiId = await res.json();
-                    this.thaiId["DOB"] = formatDate(this.thaiId["DOB"]);
-                    this.thaiId["Issue"] = formatDate(this.thaiId["Issue"]);
-                    this.thaiId["Expiry"] = formatDate(this.thaiId["Expiry"]);
-                    console.log(this.thaiId);
-                } else {
-                    throw new Error('Network response was not ok.');
-                }
-            } catch (err) {
-                this.invalid = true;
-                this.loading = false;
-                console.log(err);
-            }
-        },
-        parseMrz: function () {
-            console.log('attempting to parse input...');
-            try {
-                const parsed = newtondevMrzParser.parse(this.mrz);
-                console.log(parsed);
-                this.passport = {
-                    "Passport": parsed.documentNumber,
-                    "First Name": parsed.names.names.map(capitalizeFirstLetter).join(' '),
-                    "Last Name": capitalizeFirstLetter(parsed.names.lastName),
-                    "DOB": formatDate(parsed.dob),
-                    "Nationality": formatCountry(parsed.nationality.abbr),
-                    "Expiry": formatDate(parsed.expiry),
-                    "Sex": parsed.sex.abbr
-                };
-            } catch (error) {
-                this.invalid = true;
-                console.log('error while performing action...');
-                console.log(error);
-            }
-        },
-        fillForm: function() {
-            let script;
-
-            if (this.currentTab == 'passport') {
-                script = `
-document.getElementById("first_name_primary").value = '${this.passport["First Name"]}';
-document.getElementById("last_name_primary").value = '${this.passport["Last Name"]}';
-document.getElementById("guest_label_birthday_primary").value = '${this.passport["DOB"]}';
-document.getElementById("guest_document_type").value = 'passport';
-document.getElementById("guest_document_number_primary").value = '${this.passport["Passport"]}';
-document.getElementById("guest_country").value = '${this.passport["Nationality"]}';
-document.getElementById("guest_document_issuing_country").value = '${this.passport["Nationality"]}';
-document.getElementById("document_expiration_date_primary").value = '${this.passport["Expiry"]}';
-${genXpath('//label[.="Sex"]/../input')}.value = '${this.passport["Sex"]}';`;
-            } else {
-                script = `
-document.getElementById("first_name_primary").value = '${this.thaiId["First Name"]}';
-document.getElementById("last_name_primary").value = '${this.thaiId["Last Name"]}';
-document.getElementById("guest_label_birthday_primary").value = '${this.thaiId["DOB"]}';
-document.getElementById("guest_document_type").value = 'dni';
-document.getElementById("guest_document_number_primary").value = '${this.thaiId["ID"]}';
-document.getElementById("guest_country").value = 'TH';
-document.getElementById("guest_document_issuing_country").value = 'TH';
-document.getElementById("guest_document_issue_date_primary").value = '${this.thaiId["Issue"]}';
-document.getElementById("document_expiration_date_primary").value = '${this.thaiId["Expiry"]}';
-document.getElementById("guest_address1_primary").value = '${this.thaiId["Address"]}';
-${genXpath('//label[.="Sex"]/../input')}.value = '${this.thaiId["Sex"]}';`;
-            }
-
-            console.log(script);
-            chrome.tabs.executeScript({ code: script });
-        },
-        reset: function() {
-            this.invalid = false;
-            this.loading = false;
-
-            this.mrz = null;
-            this.passport = null;
-            this.thaiId = null;
-        },
+    parseMrz: function () {
+      console.log('attempting to parse input...');
+      try {
+        const parsed = newtondevMrzParser.parse(this.mrz);
+        console.log(parsed);
+        this.passport = {
+          "Passport": parsed.documentNumber,
+          "First Name": parsed.names.names.map(capitalizeFirstLetter).join(' '),
+          "Last Name": capitalizeFirstLetter(parsed.names.lastName),
+          "DOB": formatDate(parsed.dob),
+          "Nationality": formatCountry(parsed.nationality.abbr),
+          "Expiry": formatDate(parsed.expiry),
+          "Sex": parsed.sex.abbr
+        };
+      } catch (error) {
+        this.invalid = true;
+        console.log('error while performing action...');
+        console.log(error);
+      }
     },
+    generateScript: function (extensionTab) {
+      let script;
+
+      if (extensionTab == 'passport') {
+        script = `
+document.getElementsByName("guest_first_name")[0].value = '${this.passport["First Name"]}';
+document.getElementsByName("guest_last_name")[0].value = '${this.passport["Last Name"]}';
+document.getElementsByName("guest_email")[0].value = 'no@gmail.com';
+document.getElementsByName("guest_birthday")[0].value = '${this.passport["DOB"]}';
+document.getElementsByName("guest_document_type")[0].value = 'passport';
+document.getElementsByName("guest_document_number")[0].value = '${this.passport["Passport"]}';
+document.getElementsByName("guest_country")[0].value = '${this.passport["Nationality"]}';
+document.getElementsByName("guest_document_issuing_country")[0].value = '${this.passport["Nationality"]}';
+document.getElementsByName("guest_document_expiration_date")[0].value = '${this.passport["Expiry"]}';
+${genXpath('//label[contains(.,"Sex")]/..//input')}.value = '${this.passport["Sex"]}';`;
+      } else {
+        script = `
+document.getElementsByName("guest_first_name")[0].value = '${this.thaiId["First Name"]}';
+document.getElementsByName("guest_last_name")[0].value = '${this.thaiId["Last Name"]}';
+document.getElementsByName("guest_email")[0].value = 'no@gmail.com';
+document.getElementsByName("guest_birthday")[0].value = '${this.thaiId["DOB"]}';
+document.getElementsByName("guest_document_type")[0].value = 'dni';
+document.getElementsByName("guest_document_number")[0].value = '${this.thaiId["ID"]}';
+document.getElementsByName("guest_country")[0].value = 'TH';
+document.getElementsByName("guest_document_issuing_country")[0].value = 'TH';
+document.getElementsByName("guest_document_issue_date")[0].value = '${this.thaiId["Issue"]}';
+document.getElementsByName("guest_document_expiration_date")[0].value = '${this.thaiId["Expiry"]}';
+document.getElementsByName("guest_address1")[0].value = '${this.thaiId["Address"]}';
+${genXpath('//label[contains(.,"Sex")]/..//input')}.value = '${this.thaiId["Sex"]}';`;
+      }
+    
+      return script;
+    },
+    fillForm: function () {
+      const script = this.generateScript(this.currentTab);
+      console.log(script);
+      chrome.tabs.executeScript({ code: script });
+    },
+    reset: function () {
+      this.invalid = false;
+      this.loading = false;
+
+      this.mrz = null;
+      this.passport = null;
+      this.thaiId = null;
+    },
+  },
 });
